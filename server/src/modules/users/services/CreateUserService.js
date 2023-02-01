5d710585b16fc94db2f5ae9e46bfa319cc7b6ef3
kassio oliveira <kassiooliveira875@gmail.com>
2023-02-01 15:20:44 -0300

    Atualizando padrão de pastas do projeto

---
+++ server/src/modules/users/services/CreateUserService.js
@@ -0,0 +1,29 @@
+const User = require("../model/User");
+const bcrypt = require("bcrypt");
+
+class CreateUserService {
+
+   async createUser(name,email,password) {
+
+    try {
+
+        const salt = await bcrypt.genSalt(10);
+        const passwordHash = await bcrypt.hash(password,salt);
+
+        const user = await User.create({
+            name:name,
+            email:email,
+            password: passwordHash,
+        });
+    
+        await user.save();
+    
+        return user;
+    } catch (error) {
+        console.log(error);
+        return new Error(error.message);
+    }
+   }
+};
+
+module.exports = CreateUserService;
