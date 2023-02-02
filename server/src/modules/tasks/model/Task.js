69ec00a19e06536dd2fc40a7e6bb8938e0fd1ae7
kassio oliveira <kassiooliveira875@gmail.com>
2023-02-02 14:42:05 -0300

    Criação do model Task

---
+++ server/src/modules/tasks/model/Task.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const TaskSchema = new Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type: String,
+        default: ""
+    },
+    important:{
+        type:Boolean,
+        default:false
+    },
+    completed:{
+        type:Boolean,
+        default:false
+    },
+    user_id:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true
+    },
+    list_id:{
+        type:mongoose.Schema.Types.ObjectId,
+        default:null
+    },
+    my_day:{
+        type:Boolean,
+        default:false
+    },
+    created_at:{
+        type:Date,
+        default: Date.now()
+    }
+
+});
+
+const Task = mongoose.model("Task",TaskSchema);
+
+module.exports = Task;
