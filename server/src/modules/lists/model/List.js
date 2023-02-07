23d9b372f0fbf12f9375ba66aedccf5789770c85
kassio oliveira <kassiooliveira875@gmail.com>
2023-02-07 14:27:08 -0300

    Criação do model List

---
+++ server/src/modules/lists/model/List.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+
+const ListSchema =new Schema({
+
+    name:{
+        type:String,
+        required:true
+    },
+    user_id:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true
+    },
+    created_at:{
+        type:Date,
+        default:Date.now()
+    }
+   
+});
+
+const List = mongoose.model("List",ListSchema);
+
+module.exports = List;
--- server/src/modules/users/model/User.js
+++ server/src/modules/users/model/User.js
@@ -22,9 +22,12 @@ const UserSchema = new Schema({
     img:{
         type:String,
         default:""
+    },
+    created_at:{
+        type:Date,
+        default: Date.now()
     }
-},
-{timestamps:true});
+});
 
 const User = mongoose.model("User",UserSchema);
 
